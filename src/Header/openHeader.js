54018fa6a182fe4fe0ed692053a65ec7df80e0bf
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2023-10-06 12:38:11 +0100

    responsive header and website structure

--- src/Header/openHeader.js
+++ src/Header/openHeader.js
@@ -4,15 +4,17 @@ const header = document.querySelector("header");
 headerButtonOpen.addEventListener("click", () => {
   if (!headerButtonOpen.classList.contains("is-active")) {
     headerButtonOpen.classList.add("is-active");
+    header.classList.add("header-open");
     gsap.to(header, { x: 0, duration: .5 })
   } else {
     headerButtonOpen.classList.remove("is-active");
+    header.classList.remove("header-open");
     gsap.to(header, { x: "-100%", duration: .5 })
   }
 })
 
 document.addEventListener("click", (e) => {
-  if (!header.contains(e.target) && !headerButtonOpen.contains(e.target)) {
+  if (!header.contains(e.target) && header.classList.contains("header-open") && !headerButtonOpen.contains(e.target)) {
     gsap.to(header, { x: "-100%", duration: .5 })
     headerButtonOpen.classList.remove("is-active");
   }

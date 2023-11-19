5ae309a1069d47ef0a6897e2a9a792f194b3503d
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2023-11-19 13:48:55 +0000

    update all sections and better tailwind css

--- src/Main/sectionGsap.js
+++ src/Main/sectionGsap.js
@@ -33,44 +33,32 @@ sectionHome.forEach((section, index) => {
   });
 });
 
-const sectionHeading = document.querySelectorAll(".section-header");
-sectionHeading.forEach(heading => {
+const sectionH3 = document.querySelectorAll("h3");
+const sectionH4 = document.querySelectorAll("h4");
+sectionH3.forEach(heading => {
+  //triggerText(xValue, skewXValue, triggerElement, markersOn)
+  triggerText("100%", 45, heading, false)
+})
+sectionH4.forEach(heading => {
   //triggerText(xValue, skewXValue, triggerElement, markersOn)
   triggerText("100%", 45, heading, false)
 })
 
-const aboutMeText = document.querySelectorAll(".about-me-text");
-aboutMeText.forEach(text => {
-  //triggerText(xValue, skewXValue, triggerElement, markersOn)
-  triggerText("-200%", -45, text, false)
-})
+
 
 const skillsText = document.querySelectorAll(".skills-text");
 //triggerText(xValue, skewXValue, triggerElement, markersOn)
 triggerText("-200%", -45, skillsText, false)
 
-const skillsContainer = document.querySelectorAll(".skills-container");
-skillsContainer.forEach(skill => {
-  gsap.to(skill, { scale: 0, opacity: 0 })
-  ScrollTrigger.create({
-    trigger: skill,
-    markers: false,
-    start: "top 92%",
-    onEnter: () => {
-      gsap.to(skill, { scale: 1, opacity: 1, duration: .5 })
-    }
-  })
-})
-
-const educationContainer = document.querySelectorAll(".education-container");
-educationContainer.forEach(container => {
-  gsap.to(container, { y: 40, opacity: 0 })
+const innerContainers = document.querySelectorAll(".inner-container");
+innerContainers.forEach(container => {
+  gsap.to(container, { scale: 0, opacity: 0 })
   ScrollTrigger.create({
     trigger: container,
     markers: false,
     start: "top 92%",
     onEnter: () => {
-      gsap.to(container, { y: 0, opacity: 1, duration: .5 })
+      gsap.to(container, { scale: 1, opacity: 1, duration: .5 })
     }
   })
 })
